848e3fddc742de100f11652fa9d0bb5744eefde9
elune <elune@THINKPAD>
2023-09-07 09:23:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv ditt namn:");
+            String name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"Hej {name}, dagens datum är: {currentDate:d}");
+
+        }
+    }
+}
